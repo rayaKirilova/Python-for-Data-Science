51fbae778f1c1bcbfd1b8787d5ead4fe051d9b16
rayaKirilova <raya.n.kirilova@abv.bg>
2025-02-06 20:07:57 +0200

    adding files

---
+++ conditional_statements_exercise/biggest_of _five_numbers.py
@@ -0,0 +1,7 @@
+first_number = int(input())
+second_number = int(input())
+third_number = int(input())
+fourth_number = int(input())
+fifth_number = int(input())
+
+print(max(first_number, second_number, third_number, fourth_number, fifth_number))
---
+++ conditional_statements_exercise/fruit_or_vegetable.py
@@ -0,0 +1,8 @@
+product = input()
+
+if product == "apple" or product == "banana" or product == "kiwi" or product == "lemon" or product == "cherry":
+    print("fruit")
+elif product == "cucumber" or product == "pepper" or product == "carrot":
+    print("vegetable")
+else:
+    print("unknown")
